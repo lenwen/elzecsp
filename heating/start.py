39360c52e3b6d528ab3b63635d794885d643d67f
Lennie Wennerlund <lennie@lempa.se>
2014-12-23 00:17:17 +0100

    database init fix

--- heating/start.py
+++ heating/start.py
@@ -42,11 +42,8 @@ import lib.sensor as sensorTypes
 
 settings = libSettings.settings()
 
-
-
-print "hej"
-
 # region Test information
+'''
 #print ("burning: " + str(settings.BoilerBurningConfiguration))
 import lib.test1 as bbb
 
@@ -54,7 +51,102 @@ print "thread init"
 thTest = bbb.testThread(1,"test",1,settings)
 thTest.start()
 settings.threads.append(thTest)
+'''
 #endregion
+
+
+
+dbcon = None
+
+
+# region Def
+
+def debug(debugtext):
+    if settings.BoxDebug:
+        with open(settings.BoxDebugFile, 'a') as file_:
+            file_.write(time.strftime("%H:%M:%S") + " - " + str(debugtext) + "\n")
+
+#region application shutdown
+def shutdown():
+    print "Application will now shutdown"
+    print "Stop all running threads"
+    for t in settings.threads:
+        print(str(t.getName()) + ' will now stop()')
+        t.stop()
+    print "Wait 5 sec for threads to end"
+    print "Threads are joining before exit"
+    for t in settings.threads:
+        t.join()
+    print "GPIO cleanup"
+    GPIO.cleanup()
+    print "Application is ready to exit"
+    if settings.boxDoRestart:
+         os.system('sudo reboot')
+    sys.exit(0)
+#endregion
+
+def signal_handler(signal, frame):
+    print 'You pressed Ctrl+C!'
+    shutdown()
+
+# region Database check
+def databasecheck():
+    dbcon = None
+    try:
+        if not os.path.isfile(settings.PathDb):
+            debug("the db file don't exist. create db file")
+            dbcon = dblite.connect(settings.PathDb)
+            with dbcon:
+                cur = dbcon.cursor()
+                cur.execute("CREATE TABLE settings(name VARCHAR, value TEXT);")
+                cur.execute("CREATE UNIQUE INDEX index_Settings_Uni_Name on settings (name);")
+                cur.execute("INSERT INTO settings VALUES('dbversion', '1');")
+                cur.execute("INSERT INTO settings VALUES('version', '0.0.0');")
+                cur.execute("INSERT INTO settings VALUES('autoshutdown', '0');")
+        else:
+            dbcon = dblite.connect(settings.PathDb)
+            debug("db file exist.")
+
+        #   open version of the database schema
+        cur = dbcon.cursor()
+        cur.execute("select value from settings where name = 'dbversion'")
+        settings.dbVersionFromDatabase = cur.fetchone()[0]
+
+        debug('Database version in database: {}'.format(settings.dbVersionFromDatabase))
+
+        if int(settings.dbVersionFromDatabase) == 0:
+            debug("Database ERROR.. application will now exit.")
+            shutdown()
+        if int(settings.dbVersionFromDatabase) < 1:
+            debug("Run database upgrade 1")
+
+
+        print "Connections is OK!"
+    except Exception:
+        print "error in db"
+        shutdown()
+#endregion
+
+
+#endregion
+
+
+
+
+
+
+
+
+#   Check if database file exist
+
+databasecheck()
+
+
+#PathDb
+
+
+
+
 
 # region Args
 
@@ -83,26 +175,9 @@ sensor_args = { 'dht11': Adafruit_DHT.DHT11,
 
 print "starting"
 
-def shutDown():
-    print "Application will now shutdown"
-    print "Stop all running threads"
-    for t in settings.threads:
-        print(str(t.getName()) + ' will now stop()')
-        t.stop()
-    print "Wait 5 sec for threads to end"
-    print "Threads are joining before exit"
-    for t in settings.threads:
-        t.join()
-    print "GPIO cleanup"
-    GPIO.cleanup()
-    print "Application is ready to exit"
-    # if boxDoRestart:
-    #     os.system('sudo reboot')
-    sys.exit(0)
 
-def signal_handler(signal, frame):
-    print 'You pressed Ctrl+C!'
-    shutDown()
+
+
 
 #   Allow CTRL + c to shutdown the system
 signal.signal(signal.SIGINT, signal_handler)
